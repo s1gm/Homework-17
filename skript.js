bae7fc95b54b7717dc4150e4e6934d0887030336
Anastasia Litvinchuk <lytvynchukmark@gmail.com>
2023-07-09 22:19:43 +0300

    Homework-17

---
+++ skript.js
@@ -0,0 +1,54 @@
+// 1)
+var age = "12"
+
+console.log(age)
+
+// 2)
+// let name = "Mark"
+
+// console.log(name)
+
+// 3)
+var isStudent = "true"
+
+console.log(isStudent)
+
+// 4)
+let myString = "Найкраща помста — величезний успіх. Френк Сінатра"
+
+console.log(myString)
+
+// 5)
+let myNumber = 10;
+
+myNumber = myNumber + 10;
+
+console.log(myNumber)
+
+// 6)
+let myNull = null;
+
+console.log(myNull);
+
+// 7)
+const name = prompt("Ваше ім'я")
+
+console.log("Hello", name)
+
+// 8)
+let isConfirm = confirm();
+
+if(isConfirm){
+   console.log("Дякую за підтвердження");
+} else { 
+  console.log("Дію відмінено!");
+}
+
+// 9)
+let isWarning = confirm("Дія небезпечна");
+
+if(isWarning){
+  console.log("Дякую за підтвердження");
+}else{
+    console.log("Дію відмінено");
+}
